07320ff04ab07977bdfe93bae909646e9af3da3d
zedricka <zedrick.ara@gmail.com>
2016-12-05 08:40:08 -1000

    Updated12/5/16

---
+++ arrays_object.js
@@ -0,0 +1,25 @@
+/*
+
+  Zedrick Ara
+  Arrays and Objects
+  Dec 5, 2016
+
+*/
+
+// variables
+// ARRAYS
+var numbers = [1,2,3,4,5,6,7,8,9,10];
+var names = ["Senior Tan", "Zakattak", "Yolo Baggins", "Ohai", "Basic Jeff"];
+var colors = ["light black", "ron burgandy", "highlighter"];
+var adj = ["smart", "special case", "yummy", "tiny whiny", "ferocious"];
+var slang = ["scrap", "kobe beef", "da kine", "li dat", "pau hana"];
+
+
+// OBJECTS
+
+var donut = {
+  toppings : ["sprinkles", "chocolate frosting", "o'hare air"],
+  fillings : "jelly",
+  shape : "square",
+  count : 12
+}
